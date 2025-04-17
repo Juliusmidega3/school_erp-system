67d306125e5c96b9a408a52c1a709042eb7948da
Julius <midegajulius3@gmail.com>
2025-04-17 13:56:11 +0300

    more

--- erp_back/backend/core/urls.py
+++ erp_back/backend/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+from .views import StudentViewSet, home
 
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
 
 urlpatterns = [
+    path('', home),
     path('', include(router.urls))
 ]
--- erp_back/backend/core/views.py
+++ erp_back/backend/core/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .models import Student
 from .serializers import StudentSerializer
+from django.http import JsonResponse
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+def home(request):
+    return JsonResponse({
+        'message': 'Welcome to Faulu School'
+    })
