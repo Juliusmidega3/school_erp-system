455814710f95b04253007b3f8c40774e771f6a44
Julius <midegajulius3@gmail.com>
2025-04-17 18:51:18 +0300

    debug

--- erp_back/backend/backend/settings.py
+++ erp_back/backend/backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'corsheaders',
-    'core'
+    'core',
+
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:5173"
+    "http://localhost:5173"
 ]
+CORS_ALLOWED_ALL_ORIGINS = True
